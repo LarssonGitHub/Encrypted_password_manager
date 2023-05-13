9114ef03b9ef28918531666564c1b118cdecdada
LarrsonGitHub <LarssonGitHub>
2023-05-13 19:29:14 +0100

    feat: listed template for rendering websites implemented

--- app/TS-frontend/frontend/view/renderer.ts
+++ app/TS-frontend/frontend/view/renderer.ts
@@ -1,9 +1,7 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process unless
-// nodeIntegration is set to true in webPreferences.
-// Use preload.js to selectively enable features
-// needed in the renderer process.
+
+import {
+  websiteObject,
+} from "../../../@types/@type-module";
 
 console.log(
   `This app is using Chrome (v${window.API.processVersion.chrome()}), Node.js (v${window.API.processVersion.node()}), and Electron (v${window.API.processVersion.electron()})`
@@ -11,3 +9,24 @@ console.log(
 
 // preload attached everything to window.
 console.log(window);
+
+// HTML tags always present, if changed, like the template html tag, remove assertion operator & update guards
+const listDataContainer: HTMLElement = document.getElementById("list-data-container")!;
+const template = (document.getElementById("template-list") as HTMLTemplateElement)!;
+
+export const appendToListTemplate = (websiteArray: websiteObject[]) : void => {
+  if (websiteArray.length < 0) return;
+  const clone = template.content.cloneNode(true) as DocumentFragment;
+  const listElement = clone.getElementById('list-data-unordered-list')! as HTMLUListElement;  
+  websiteArray.forEach(i => {
+    let newClone = listElement.cloneNode(true) as DocumentFragment; 
+    newClone.querySelector('.list-id')!.textContent = i.id;
+    newClone.querySelector('.list-website')!.textContent = i.websiteInput;
+    newClone.querySelector('.list-username')!.textContent = i.emailInput;
+    newClone.querySelector('.list-email')!.textContent = i.usernameInput;
+    newClone.querySelector('.list-password')!.textContent = i.passwordInput;
+    newClone.querySelector('.list-additional-data')!.textContent = i.additionalDataInput;
+    listDataContainer.append(newClone);
+  })
+
+}
