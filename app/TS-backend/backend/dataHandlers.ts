5f4846bdb58e3f1f8ab1bdc20e6834feb6a7706e
LarrsonGitHub <LarssonGitHub>
2023-05-15 20:28:14 +0100

    fix: corrected bad if statements and logic

--- app/TS-backend/backend/dataHandlers.ts
+++ app/TS-backend/backend/dataHandlers.ts
@@ -8,8 +8,6 @@ import {
   InsertIntoWebsitesArray,
   removeItemWebsiteArray,
   updateItemWebsiteArray,
-  decryptData,
-  encryptData,
   createResponse,
   sanitizeEncryptedData,
   encryptAndInsertDatabaseData
@@ -19,44 +17,50 @@ import {
 } from "./fileSystem"
 
 export const getData = async (key: string): Promise < customResponse > => {
+    if (!key || key.length === 0)
+        throw createResponse(false, "No key was submitted")
   const encryptedData: string = await getDatabaseData();
-      if (encryptedData.length === 0) return createResponse(true, "Your database is empty", null)
+    if (encryptedData.length === 0) 
+        return createResponse(true, "Your database is empty", null)
   const decryptedData: arrayOfWebsites = sanitizeEncryptedData(encryptedData, key)
-  return createResponse(true, "Retrieved data from database", decryptedData)
+  return createResponse(true, "Get request successful", decryptedData)
 }
 
-// Create one encrypt and decrypt function...! 
-export const deleteData = async (objectId: string, key: string): Promise <customResponse> => {
-      if (!objectId || objectId.length === 0) 
-          throw createResponse(false, "No id was given, canceling event", null);
+export const deleteData = async (objectId: string, key: string): Promise < customResponse > => {
+    if (!objectId || objectId.length === 0)
+        throw createResponse(false, "No id was given, canceling event", null);
+    if (!key || key.length === 0)
+        throw createResponse(false, "No key was submitted")
   const encryptedData: string = await getDatabaseData();
-      if (encryptedData.length === 0) 
-          throw createResponse(false, "Your database is empty, canceling delete request", null);
+    if (encryptedData.length === 0)
+        throw createResponse(false, "Your database is empty, canceling delete request", null);
   const decryptedData: arrayOfWebsites = sanitizeEncryptedData(encryptedData, key);
-      if (Array.isArray(decryptedData)) 
-          throw createResponse(false, "The data from database wasn't an array, canceling delete request", null);
+    if (!Array.isArray(decryptedData))
+        throw createResponse(false, "The data from database wasn't an array, canceling delete request", null);
   const updatedData: arrayOfWebsites = removeItemWebsiteArray(objectId, decryptedData);
-      if (!Array.isArray(updatedData)) 
-          throw createResponse(false, "Couldn't update the data, canceling delete request", null);
+    if (!Array.isArray(updatedData))
+        throw createResponse(false, "Couldn't update the data, canceling delete request", null);
   const updatedDatabase: boolean = await encryptAndInsertDatabaseData(updatedData, key)
-      if (!updatedDatabase) 
-          throw createResponse(false, "Couldn't write to Database", null);
-  return createResponse(true, "Delete successful", updatedData);
+    if (!updatedDatabase)
+        throw createResponse(false, "Couldn't write to Database", null);
+  return createResponse(true, "Delete request successful", updatedData);
 }
 
-export const postData = async (postData: websiteObject, key: string): Promise <customResponse> => {
+export const postData = async (postData: websiteObject, key: string): Promise < customResponse > => {
+    if (!postData || Object.values(postData).every(x => x === null || x === ''))
+        throw createResponse(false, "No data was submitted")
   const updatePostDataId: websiteObject = createAndAppendId(postData);
+    if (!key || key.length === 0)
+        throw createResponse(false, "No key was submitted")
   const encryptedData: string = await getDatabaseData();
-      if (encryptedData.length === 0) 
-          return createResponse(true, "Your database is empty", null)
-  const decryptedData: arrayOfWebsites = sanitizeEncryptedData(encryptedData, key)
-      if (Array.isArray(decryptedData)) 
-          throw createResponse(false, "The data from database wasn't an array, canceling post request", null);
-  const postedData: arrayOfWebsites =  InsertIntoWebsitesArray(updatePostDataId, decryptedData)
-      if (!Array.isArray(postedData)) 
-          throw createResponse(false, "Couldn't update the data, canceling post request", null);
+  const decryptedData: arrayOfWebsites | [] = encryptedData.length > 0 ? sanitizeEncryptedData(encryptedData, key) : []
+    if (!Array.isArray(decryptedData))
+        throw createResponse(false, "The data from database wasn't an array, canceling post request", null);
+  const postedData: arrayOfWebsites = InsertIntoWebsitesArray(updatePostDataId, decryptedData)
+    if (!Array.isArray(postedData))
+        throw createResponse(false, "Couldn't update the data, canceling post request", null);
   const updatedDatabase: boolean = await encryptAndInsertDatabaseData(postedData, key)
-        if (!updatedDatabase) 
-            throw createResponse(false, "Couldn't write to Database", null);
-  return createResponse(true, "Delete successful", postedData);
+    if (!updatedDatabase)
+        throw createResponse(false, "Couldn't write to Database", null);
+  return createResponse(true, "Post request successful", postedData);
 }
