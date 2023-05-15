f47af208d35442b128886f5f14bd46b290f1a2e4
LarrsonGitHub <LarssonGitHub>
2023-05-15 18:05:37 +0100

    feat: updated post data func with working error handling

--- app/TS-backend/backend/dataHandlers.ts
+++ app/TS-backend/backend/dataHandlers.ts
@@ -38,8 +38,25 @@ export const deleteData = async (objectId: string, key: string): Promise <custom
   const updatedData: arrayOfWebsites = removeItemWebsiteArray(objectId, decryptedData);
       if (!Array.isArray(updatedData)) 
           throw createResponse(false, "Couldn't update the data, canceling delete request", null);
-  const updatedDatabase: boolean = await encryptAndInsertDatabaseData(encryptedData, key)
-  if (!updatedDatabase) 
-      throw createResponse(false, "Couldn't write to Database", null);
-  return createResponse(true, "Delete successful", updatedData);;
+  const updatedDatabase: boolean = await encryptAndInsertDatabaseData(updatedData, key)
+      if (!updatedDatabase) 
+          throw createResponse(false, "Couldn't write to Database", null);
+  return createResponse(true, "Delete successful", updatedData);
+}
+
+export const postData = async (postData: websiteObject, key: string): Promise <customResponse> => {
+  const updatePostDataId: websiteObject = createAndAppendId(postData);
+  const encryptedData: string = await getDatabaseData();
+      if (encryptedData.length === 0) 
+          return createResponse(true, "Your database is empty", null)
+  const decryptedData: arrayOfWebsites = sanitizeEncryptedData(encryptedData, key)
+      if (Array.isArray(decryptedData)) 
+          throw createResponse(false, "The data from database wasn't an array, canceling post request", null);
+  const postedData: arrayOfWebsites =  InsertIntoWebsitesArray(updatePostDataId, decryptedData)
+      if (!Array.isArray(postedData)) 
+          throw createResponse(false, "Couldn't update the data, canceling post request", null);
+  const updatedDatabase: boolean = await encryptAndInsertDatabaseData(postedData, key)
+        if (!updatedDatabase) 
+            throw createResponse(false, "Couldn't write to Database", null);
+  return createResponse(true, "Delete successful", postedData);
 }
